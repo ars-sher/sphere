230cb04887d0a285fa63fa72bfa24366330b184a
Arseny Sher <arsv.sher@gmail.com>
2016-02-27 21:05:26 +0300

    ejudje accepted

--- NumeralSystemsConverter/main.c
+++ NumeralSystemsConverter/main.c
@@ -1,22 +1,42 @@
 #include <stdio.h>
+#include <stdint.h>
 #include <stdlib.h>
 #include <ctype.h>
 #include <stdarg.h>
+#include <limits.h>
+
+int silent = 1;
 
 void log_error(const char *errformat, ...) {
-    va_list args;
-    va_start(args, errformat);
-    printf("Error: " );
-    vprintf(errformat, args);
-    printf("\n");
-    va_end(args);
+    if (!silent) {
+        va_list args;
+        va_start(args, errformat);
+        printf("Error: ");
+        vprintf(errformat, args);
+        printf("\n");
+        va_end(args);
+    }
 }
 
 void error(const char *errformat, ...) {
-    va_list args;
-    va_start(args, errformat);
-    log_error(errformat, args);
-    va_end(args);
+    if (!silent) {
+        va_list args;
+        va_start(args, errformat);
+        printf("Error: "); //TODO
+        vprintf(errformat, args);
+        printf("\n");
+        va_end(args);
+    }
+}
+
+void error_exit(int n_vectors_to_free, ...) {
+
+    va_list valist;
+    va_start(args, n_vectors_to_free);
+    for (int i = 0; i< n_vectors_to_free; i++){
+        vector_free(va_arg(valist, CharVector*));
+    }
+    fprintf(stdout, "[error]");
     exit(0);
 }
 
@@ -38,14 +58,26 @@ void *erealloc(void* ptr, size_t size, const char *errstr) {
     return v;
 }
 
+unsigned long long ull_safe_add(unsigned long long x, unsigned long long y) {
+    if (ULLONG_MAX - x <= y)
+        error("unsigned long long add overflow");
+    return x + y;
+}
+
+unsigned long long ull_safe_mul(unsigned long long x, unsigned long long y) {
+    if (x != 0 && y != 0 && (ULLONG_MAX / x) + 1 <= y) {
+        error("unsigned long long multiplication overflow");
+    }
+    return x * y;
+}
+
 char letter_to_dec(char letter) {
   if (isalpha(letter))
       return (char)tolower(letter) - (char)'a' + (char)10;
   else if (isdigit(letter))
       return letter - (char)'0';
-  else {
-      error("symbol %c can't be converted to decimal", letter);
-  }
+  error("symbol %c can't be converted to decimal", letter);
+  return 0; // must never reach this
 }
 
 char dec_to_letter(char dec) {
@@ -78,6 +110,8 @@ CharVector vector_construct() {
 
 void vector_check_and_extend(CharVector *vector) {
     if (vector->size == vector->capacity) {
+        if (vector->capacity >= (SIZE_MAX - 1) / 2)
+            error("CharVector error: vectors of size more than SIZE_MAX/2 are not supported");
         vector->capacity *= 2;
         vector->data = erealloc(vector->data, sizeof(*(vector->data)) * vector->capacity,
                                 "CharVector extension failed");
@@ -138,7 +172,6 @@ void swap_chars(char *c1, char*c2) {
 void vector_reverse(CharVector* vector) {
     char* start = vector->data;
     char* end = vector->data + vector->size - 1;
-    printf("%d\n", end);
     for (size_t i = 0; i < vector->size / 2; i++)
         swap_chars(start++, end--);
 }
@@ -155,26 +188,114 @@ void vector_print(CharVector *vector) {
     printf("\n");
 }
 
-CharVector convert(CharVector src, char src_base, char dst_base) {
-    
+void vector_print_letters(CharVector *vector) {
+    for (size_t i = 0; i < vector->size; i++)
+        printf("%c", dec_to_letter(vector->data[i]));
 }
 
-int main() {
-    CharVector chv = vector_from_str("abc5def9yy");
-    vector_print(&chv);
-    vector_reverse(&chv);
-    swap_chars(chv.data, chv.data + 1);
-    vector_print(&chv);
-    vector_free(&chv);
-    printf("%d\n", letter_to_dec('h'));
-    printf("Hello, World!\n");
+unsigned long long chv_to_number(CharVector* chv, char base) {
+  unsigned long long res = 0;
+  if (chv->size == 0)
+      return 0;
+  if (chv->size == 1)
+      return chv->data[0];
+  res = chv->data[0];
+  for (size_t i = 1; i < chv->size; i++)
+      res = ull_safe_add(ull_safe_mul(base, res), chv->data[i]);
+  return res;
+}
+
+CharVector number_to_chv(unsigned long long number, char base) {
+    CharVector res_chv = vector_construct();
+    char remainder = base;
+    while (number >= base) {
+        remainder = number % base;
+        number = number / base;
+        vector_append(&res_chv, remainder);
+//        printf("appened remainder is %d and number is %llu\n", remainder, number);
+    }
+    if (number != 0)
+        vector_append(&res_chv, number);
+    vector_reverse(&res_chv);
+    return res_chv;
+}
+
+CharVector convert(CharVector* src, char src_base, char dst_base) {
+    unsigned long long src_number = chv_to_number(src, src_base);
+//    printf("and the number is %llu\n", src_number);
+    return number_to_chv(src_number, dst_base);
+}
+
+void show_usage() {
+    printf("Usage: main <src_base> <dst_base> <number>\n");
+}
+
+long stolerrcheck(char* str) {
+    char *endptr;
+    long value = strtol(str, &endptr, 10);
+    if (endptr == str || *endptr != '\0')
+        error("The src and dst base must be int");
+    return value;
+}
+
+// we would need that in case of passing args to executable
+CharVector parse_args(int argc, char *argv[], char* psrc_base, char* pdst_base) {
+    if (argc != 4) {
+        show_usage();
+        error("Number of arguments %d is not equal 4", argc);
+    }
+    *psrc_base = stolerrcheck(argv[1]);
+    *pdst_base = stolerrcheck(argv[2]);
+    if (!(2 <= *pdst_base && *pdst_base < *psrc_base && *psrc_base <= 36))
+        error("Bases must be: 2 <= dst_base < src_base <= 36");
+    CharVector src_number_chv = vector_construct();
+    char* number_str = argv[3];
+    while (*number_str ) {
+        char dec = letter_to_dec(*number_str);
+        if (dec >= *psrc_base)
+            error("Numeral system with base %d can't contain letter %c", *psrc_base, *number_str);
+        vector_append(&src_number_chv, dec);
+        number_str++;
+    }
+    return src_number_chv;
+}
+
+CharVector read_args(char* psrc_base, char* pdst_base) {
+    int src_base; int dst_base;
+    int res = scanf("%d", &src_base);
+    if (res == EOF || res == 0)
+        error("Read crap instead of src_base");
+    res = scanf("%d", &dst_base);
+    if (res == EOF || res == 0)
+        error("Read crap instead of dst_base");
+    if (!(2 <= dst_base && dst_base < src_base && src_base <= 36))
+        error("Bases must be: 2 <= dst_base < src_base <= 36");
+    *psrc_base = (char) src_base;
+    *pdst_base = (char) dst_base;
+    char c;
+    CharVector src_number_chv = vector_construct();
+    while ((c = getchar()) != EOF && isspace(c));
+    ungetc(c, stdin);
+    while ((c = getchar()) != EOF) {
+        if (isspace(c))
+            error("number string expected");
+        char dec = letter_to_dec(c);
+        if (dec >= *psrc_base)
+            error("Numeral system with base %d can't contain letter %c", *psrc_base, c);
+        vector_append(&src_number_chv, dec);
+    }
+    if (src_number_chv.size == 0)
+        error("number string expected");
+    return src_number_chv;
+}
+
+int main(int argc, char *argv[]) {
+    char src_base; char dst_base;
+//    CharVector src_number_chv = parse_args(argc, argv, &src_base, &dst_base);
+    CharVector src_number_chv = read_args(&src_base, &dst_base);
+    CharVector res_chv = convert(&src_number_chv, src_base, dst_base);
+    vector_print_letters(&res_chv);
+    vector_free(&src_number_chv);
+    vector_free(&res_chv);
     return 0;
 }
-
-// add one digit in system x to another; (char, char, x) -> (char, char). first res char is 0 or 1, in fact
-
-// add one number in system x to another; (char*, char* ) -> char*
-
-// multiply one number in system x to another; (char*, char*) -> char*
-
-// conversion, likely recursive (int, int, char*) -> char*
